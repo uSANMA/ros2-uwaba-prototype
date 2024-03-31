5eda27b20039e2e34cc9c25d05ba530ee9e3a55e
Francisco Martins Junior <franciscosdm.jr@gmail.com>
2024-03-31 15:45:31 -0300

    v0.0.2

--- packages/build/uwaba_prototype_description/cmake_install.cmake
+++ packages/build/uwaba_prototype_description/cmake_install.cmake
@@ -43,73 +43,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description/" TYPE DIRECTORY FILES
-    "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/launch"
-    "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/meshes"
-    "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/ros2_control"
-    "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/rviz"
-    "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/urdf"
-    "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/config"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/uwaba_prototype_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/uwaba_prototype_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description/environment" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/ament_prefix_path.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description/environment" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/path.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/local_setup.bash")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/local_setup.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/local_setup.zsh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/local_setup.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_environment_hooks/package.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_index/share/ament_index/resource_index/packages/uwaba_prototype_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description/cmake" TYPE FILE FILES
-    "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_core/uwaba_prototype_descriptionConfig.cmake"
-    "/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_core/uwaba_prototype_descriptionConfig-version.cmake"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/uwaba_prototype_description" TYPE FILE FILES "/home/frantches/Workspaces/ros2_ws/packages/src/uwaba_prototype_description/package.xml")
+  include("/home/frantches/Workspaces/ros2_ws/packages/build/uwaba_prototype_description/ament_cmake_symlink_install/ament_cmake_symlink_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
