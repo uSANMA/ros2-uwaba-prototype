9bd3db8ed26eeeaed8b038ca71c9a0bc3a204205
Francisco Martins Junior <franciscosdm.jr@gmail.com>
2024-04-07 20:52:46 -0300

    v0.0.3

---
+++ packages/src/uwaba_prototype_testes/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'uwaba_prototype_testes'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='frantches',
+    maintainer_email='frantches@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "uros_std_msg_teste = uwaba_prototype_testes.uros_std_msg_teste:main"
+        ],
+    },
+)
---
+++ packages/src/uwaba_prototype_testes/uwaba_prototype_testes/uros_std_msg_teste.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+ 
+ 
+class UrosTestNode(Node):
+    def __init__(self):
+        super().__init__("Uros_Test_Node")
+        self.msg_ = Int32()
+        self.msg_.data = 0
+        self.timer_freq_ = 1
+        self.data_publisher_ = self.create_publisher(Int32, "/motor/cmd", 10)
+        self.data_publisher2_ = self.create_publisher(Int32, "Encoder_Msgs", 10)
+        self.data_subscriber_ = self.create_subscription(Int32, "/motor/encoder", self.subscription_cb, 10)
+        self.timer_ = self.create_timer(1/self.timer_freq_, self.publish_cmd)
+        
+    def publish_cmd(self):
+        self.msg_.data += 1
+        self.data_publisher_.publish(self.msg_)
+        self.get_logger().info("Dado: " + str(self.msg_))
+        
+    def subscription_cb(self, msg: Int32):
+        self.data_publisher2_.publish(msg)
+        self.get_logger().warn("Dado recebido: " + str(msg))
+
+ 
+def main(args=None):
+    rclpy.init(args=args)
+    node = UrosTestNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+ 
+ 
+if __name__ == "__main__":
+    main()
