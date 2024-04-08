b5f5520d8d4b998d95a6377eb36f763402146949
Francisco Martins Junior <franciscosdm.jr@gmail.com>
2024-04-07 21:41:10 -0300

    Removed build/ install/ and log/ directories from remote.

---
+++ packages/build/uwaba_prototype_testes/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/frantches/ros2-uwaba-prototype/packages/install/uwaba_prototype_testes'
---
+++ packages/build/uwaba_prototype_testes/share/uwaba_prototype_testes/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/frantches/ros2-uwaba-prototype/packages/build/uwaba_prototype_testes"
