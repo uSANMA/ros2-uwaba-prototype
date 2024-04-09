26b23d7fb1fa4f23ab28fef223b8213a84ba94d2
Francisco Martins Junior <franciscosdm.jr@gmail.com>
2024-04-09 13:20:15 -0300

    Removed build, install and log

--- packages/build/uwaba_prototype_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/frantches/ros2-uwaba-prototype/packages/src/uwaba_prototype_description
-# Build directory: /home/frantches/ros2-uwaba-prototype/packages/build/uwaba_prototype_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
